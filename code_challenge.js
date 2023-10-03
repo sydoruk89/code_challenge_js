b4cfa1c5192b2d01b4fe85d4f009443c66725d90
Roman <sydoruk89@gmail.com>
2023-10-02 21:32:02 -0700

    tests added

--- code_challenge.js
+++ code_challenge.js
@@ -2,15 +2,23 @@ const checkVowels = (word)=>{
     const vowels = ["a", "e", "i", "o", "u"];
     let count = 0;
     for (let i = 0; i<word.length; i++){
-        if(vowels.includes(word[i])){
+        if(vowels.includes(word[i].toLowerCase())){
             count++;
         }
     }
     return count;
 }
 const longestWord = (sent)=>{
-    const clearSentance = sent.replace(/[^a-zA-Z0-9\s]/g, ''); 
+    if(!sent){
+        return
+    }
+    const clearSentance = sent.replace(/[^a-zA-Z\s]/g, ''); 
     const words = clearSentance.split(" ")
+    if(!words){
+        return
+    } else if (!words[1]){
+        return words[0]
+    }
     const sortedWords = words.sort((a,b)=> b.length - a.length);
     if (!(sortedWords[0].length === sortedWords[1].length)){
         return sortedWords[0]
@@ -24,4 +32,20 @@ const longestWord = (sent)=>{
     return longestWords[0]
 }
 const sent  = "Smart people learn from everything and everyone, average people from their, stupid people already, have all the answers"
+// test 1 - "Smart people learn from everything and everyone, average people from their, stupid people already, have all the answers"
+// test 2 - "1"
+// test 3 - "0"
+// test 4 - "," "." "?" "!"
+// test 5 - " "
+// test 6 - "apple"
+// test 7 - "apple1"
+// test 8 - "apple,"
+// test 9 - "apple_"
+// test 10 - "apple, peach"
+// test 11 - "apple_peach"
+// test 12 - "apple peach"
+// test 13 - "APPLE"
+// test 14 - "ApPle"
+// test 13 - try foreign language
+// test 14 - huge text
 console.log(longestWord(sent))
